de131b207f9b21821c601609ebed81466b948edf
arwinlashawn <aanakkendary@ucdavis.edu>
2021-11-21 22:05:51 +0800

    [add] updated readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # ngrammer
-Some NPL stuff.
+Some NLP stuff.
+
+# Usage
+The API takes a body as follows:
+```
+{
+    "order": 2,
+    "text": "This is a beautiful world."
+}
+```
+
+# Expected performance of the API with respect to the payload size
+The API performance degrades with increasing payload size. Did a bit of time profiling to see how the API slows down as text length increases by 10x, 100x, 1000x, and so on. Here are the results:
+
+```
+text1 = "This is a beautiful world. "
+text2 = text1*10 # 0.66x slower than text1 processing time
+text3 = text1*100 # 3.5x slower than text1 processing time
+text4 = text1*1000 # 86x slower than text1 processing time
+text5 = text1*10000 # 1200x slower than text1 processing time
+text6 = text1*100000 # 4400x slower than text1 processing time
+```
+
+# Ways to improve the response time of the API considering inputs will mostly be long essays
+* Implement caching of API response
+* Increase memory allocation for Lambda, which increases compute speed
+* Consider the use of `colibricore` library for faster n-gram sequence determination
+
--- app.py
+++ app.py
@@ -1,36 +1,22 @@
 from chalice import Chalice
+from nltk import ngrams
 
 app = Chalice(app_name='ngrammer')
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+@app.route("/")
+def test():
+    return {"hello": "world"}
 
-@app.route('/ngrammer', methods=['POST'])
+@app.route("/ngrammer", methods=["POST"])
 def ngrammer():
-    # This is the JSON body the user sent in their POST request.
-    user_as_json = app.current_request.json_body
-    # We'll echo the json body back to the user in a 'user' key.
-    return {'user': user_as_json}
+    # to do: implement error handling, black and flake8 formatting
+    json_input = app.current_request.json_body
+    order = json_input["order"]
+    text = json_input["text"]
+    result = {i for i in ngrams(text.split(), order)}
 
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    return {
+    "order": order,
+    "ngrams": result
+    }
